609e9d5dc5c31f698d4364b6c70850c256139f91
Gustavo Gomes <contato.gustavo314@gmail.com>
2026-06-09 21:04:19 -0300

    Create Veiculos.cs with the base properties

---
+++ Analise-Veiculos-Usados/Analise-Veiculos-Usados/Models/Veiculo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+                                            // ARQUIVO DE ESTRUTURAÇÃO DA CLASSE VEICULO //
+namespace Analise_Veiculos_Usados.Models
+{
+    public class Veiculo
+    {
+        public string Modelo { get; set; }
+        public string Versao { get; set; }
+        public string Fabricante { get; set; }
+        public string Combustivel { get; set; }
+        public int AnoFabricacao { get; set; }
+        public int AnoModelo { get; set; }
+        public string Placa { get; set; }
+        public string Chassi { get; set; }
+        public string NotaFiscal { get; set; }
+        public DateTime DataNF { get; set; }
+        public decimal ValorNF { get; set; }
+
+
+    }
+            
+           
+
+
+            
+
+
+
+    
+}
